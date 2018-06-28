4879188fdb6d1929daa77b932aad6177fad58d0e
Mikael Edebro <mikael.edebro@gmail.com>
2018-06-28 10:43:40 +0200

    Serve frontend in production

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require("path");
 
 app.get("/", (req, res) => {
   res.send({ helloo: true });
 });
+
+// serve up react app in prod
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT);
