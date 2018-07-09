8f4850b34b91271f6d090ecb8f732d718d85fbbb
Mikael Edebro <mikael.edebro@gmail.com>
2018-07-10 00:27:56 +0200

    Update prod api base url

--- client/src/axios-instance.js
+++ client/src/axios-instance.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+const isProduction = process.env.NODE_ENV === 'production'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: isProduction ? 'https://trubadur-app.herokuapp.com/' : 'http://localhost:3000',
 })
 
 export default instance
