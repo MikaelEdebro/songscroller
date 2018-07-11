4717139f4d3cb08d1404a41e6f464923385ee335
Mikael Edebro <mikael.edebro@gmail.com>
2018-07-11 17:24:29 +0200

    Update endpoint

--- client/src/axios-instance.js
+++ client/src/axios-instance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const isProduction = process.env.NODE_ENV === 'production'
 
 const instance = axios.create({
-  baseURL: isProduction ? 'https://trubadur-app.herokuapp.com/' : 'http://localhost:3000',
+  baseURL: isProduction ? 'http://www.songscroller.io/' : 'http://localhost:3000',
 })
 
 export default instance
