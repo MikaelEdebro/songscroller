c10dfe3adac495cf8bfe23eecd176b607c15ef76
Mikael Edebro <mikael.edebro@gmail.com>
2018-08-29 23:55:36 +0200

    Path to static assets

--- app.ts
+++ app.ts
@@ -43,7 +43,7 @@ app.use('/api', authController, userController, songController, playlistControll
 
 // serve up react app in prod
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'))
+  app.use(express.static(path.resolve(__dirname, '../client/build')))
 
   app.get('*', (req: Request, res: Response) => {
     res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
