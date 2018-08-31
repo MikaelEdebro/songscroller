4d926916337ace752f991174ba57950d1e2348d0
Mikael Edebro <mikael.edebro@gmail.com>
2018-08-31 11:19:27 +0200

    Use any for req, since it was causing issues with ts-node.

--- server/src/controllers/songController.ts
+++ server/src/controllers/songController.ts
@@ -4,15 +4,13 @@ import validateSong from '../middlewares/validation/validateSong'
 import { songService } from '../services'
 import to from 'await-to-js'
 import { validationResult } from 'express-validator/check'
-import { Song } from '../models'
 
 const router = Router()
 
-router.get('/songs', requireLogin, async (req: Request, res: Response, next: NextFunction) => {
-  const userId = req.user._id
+router.get('/songs', requireLogin, async (req: any, res: Response, next: NextFunction) => {
   let err
   let songs
-  ;[err, songs] = await to(songService.getSongsByUser(userId.toString()))
+  ;[err, songs] = await to(songService.getSongsByUser(req.user._id.toString()))
   if (err) {
     return next(err)
   }
@@ -20,7 +18,7 @@ router.get('/songs', requireLogin, async (req: Request, res: Response, next: Nex
   res.send(songs)
 })
 
-router.get('/songs/:id', requireLogin, async (req: Request, res: Response, next: NextFunction) => {
+router.get('/songs/:id', requireLogin, async (req: any, res: Response, next: NextFunction) => {
   const song = await songService.getSongById(req.user._id.toString(), req.params.id)
 
   res.send(song)
@@ -30,7 +28,7 @@ router.post(
   '/songs',
   requireLogin,
   validateSong,
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: any, res: Response, next: NextFunction) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() })
@@ -51,7 +49,7 @@ router.put(
   '/songs/:id',
   requireLogin,
   validateSong,
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: any, res: Response, next: NextFunction) => {
     const { artist, title, body, useMonospaceFont } = req.body
     const songValues = {
       artist,
@@ -70,20 +68,16 @@ router.put(
   }
 )
 
-router.delete(
-  '/songs/:id',
-  requireLogin,
-  async (req: Request, res: Response, next: NextFunction) => {
-    let err
-    let song
-    ;[err, song] = await to(songService.delete(req.user._id, req.params.id))
+router.delete('/songs/:id', requireLogin, async (req: any, res: Response, next: NextFunction) => {
+  let err
+  let song
+  ;[err, song] = await to(songService.delete(req.user._id, req.params.id))
 
-    if (err) {
-      return next(err)
-    }
-
-    res.status(200).send(song)
+  if (err) {
+    return next(err)
   }
-)
+
+  res.status(200).send(song)
+})
 
 export default router
